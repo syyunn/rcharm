58847e94d84e9e00e7e8e51048597d42cc37986b
Suyeol Yun <syyun@mit.edu>
2021-07-22 18:14:45 +0900

    vector handling

---
+++ tutorial/math.R
@@ -0,0 +1,17 @@
+# create vector
+vec1 = c(1:20)
+
+# remove 3rd element
+vec1 <- vec1[-3]
+
+# add 2 to remaining elements
+vec1 <- vec1 + 2
+
+# multply vector bt 5
+vec1 <- vec1 * 5
+
+# take sqrt for each elem
+vec1 <- sqrt(vec1)
+
+# take natural log per elem
+vec1 <- log(vec1)
